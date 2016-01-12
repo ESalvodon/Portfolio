c9788d93828b9046584567327861e636a1ed4f2b
Eddy Salvodon <Esalvodon@Gmail.com>
2016-01-12 13:28:46 -0500

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,18 @@
 import $ from 'jquery';
-import Backbone from 'backbone';
-import _ from 'underscore';
 
-$(() => {
-  // Ready!
+$(function() {
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 });
